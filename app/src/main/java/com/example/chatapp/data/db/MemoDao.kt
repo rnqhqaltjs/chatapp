8c89cbb0ea0c5eac02ca7e8b8c78040a350bf4ea
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-11-19 02:01:53 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/chatapp/data/db/MemoDao.kt
@@ -0,0 +1,48 @@
+package com.example.chatapp.data.db
+
+import androidx.room.*
+import com.example.chatapp.data.model.Memo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MemoDao {
+
+    // OnConflictStrategy.IGNORE = 동일한 아이디가 있을 시 무시
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addMemo(memo : Memo)
+
+    @Update
+    suspend fun updateMemo(memo : Memo)
+
+    @Delete
+    suspend fun deleteMemo(memo : Memo)
+
+    // 큰 날짜부터 출력
+    @Query("SELECT * FROM Memo ORDER BY year and month and day")
+    fun readAllData() : Flow<List<Memo>>
+
+    // 날짜 정보를 입력받아 그 날짜에 해당하는 메모만 반환
+    @Query("SELECT * FROM Memo WHERE year = :year AND month = :month AND day = :day")
+    fun readDateData(year : Int, month : Int, day : Int) : List<Memo>
+
+    // 완료한 메모만 출력
+    @Query("SELECT * FROM Memo WHERE `check` = 1 ORDER BY year DESC, month DESC, day DESC, id DESC")
+    fun readDoneData() : Flow<List<Memo>>
+
+    // 캘린더에 모든 날짜 가져옴
+    @Query("SELECT * FROM Memo ")
+    fun getCalendarAll() : List<Memo>
+
+    // 모든 오늘 날짜를 가져옴
+    @Query("SELECT * FROM Memo WHERE year = :year AND month = :month AND day = :day")
+    fun getTodayAll(year : Int, month : Int, day : Int) : List<Memo>
+
+    // 모든 오늘 날짜 중에 완료된 날짜만 가져옴
+    @Query("SELECT * FROM Memo WHERE `check` = 1 AND year = :year AND month = :month AND day = :day")
+    fun getCompletion(year : Int, month : Int, day : Int) : List<Memo>
+
+    //모든 데이터를 제거
+    @Query("Delete FROM Memo ")
+    fun deleteAllMemo()
+
+}
---
+++ app/src/main/java/com/example/chatapp/data/model/Memo.kt
@@ -0,0 +1,17 @@
+package com.example.chatapp.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Memo(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    val check: Boolean,
+    val title: String,
+    val content: String?,
+    val year: Int,
+    val month: Int,
+    val day: Int,
+)

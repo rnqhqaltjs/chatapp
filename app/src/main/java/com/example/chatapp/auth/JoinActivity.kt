74ee3569312b78aca17ec231d2c7f0e8f433a9fa
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-11-19 00:06:06 +0900

    Initial Commit

--- app/src/main/java/com/example/chatapp/auth/JoinActivity.kt
+++ app/src/main/java/com/example/chatapp/auth/JoinActivity.kt
@@ -1,12 +1,80 @@
 package com.example.chatapp.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.chatapp.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chatapp.MainActivity
+import com.example.chatapp.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private val binding: ActivityJoinBinding by lazy {
+        ActivityJoinBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.joinBtn.setOnClickListener {
+
+            val email = binding.emailArea.text.toString()
+            val password1 = binding.passwordArea1.text.toString()
+            val password2 = binding.passwordArea2.text.toString()
+
+            var isGoToJoin = true
+
+            if(email.isEmpty()){
+                Toast.makeText(this,"이메일을 입력해 주세요",Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            if(password1.isEmpty()){
+                Toast.makeText(this,"패스워드1을 입력해 주세요",Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            if(password2.isEmpty()){
+                Toast.makeText(this,"패스워드2을 입력해 주세요",Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            if(password1 != password2){
+                Toast.makeText(this,"패스워드를 똑같이 입력해 주세요",Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+
+            if(password1.length<6){
+                Toast.makeText(this,"패스워드를 6자리 이상 입력해주세요",Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+
+            if(isGoToJoin){
+                auth.createUserWithEmailAndPassword(email, password1)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this,"성공",Toast.LENGTH_SHORT).show()
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this,"실패",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }
+
+        }
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/chatapp/auth/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/auth/LoginActivity.kt
@@ -1,12 +1,52 @@
 package com.example.chatapp.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.chatapp.MainActivity
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivityJoinBinding
+import com.example.chatapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        auth=Firebase.auth
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this,"성공",Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+
+                    } else {
+
+                        Toast.makeText(this,"실패",Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+        }
     }
 }
